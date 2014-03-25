2eb48ec9aacfe9ad2d43c3c1490b2734aa946730
Martin J <martinnjorgensen@gmail.com>
2014-03-25 13:07:41 +0100

    Added new sub-project.
    CommonGui is supposed to contain custom made GUI elements that might be
    common for both the reader and editor.

---
+++ CommonGui/VarEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonGui
+{
+    public partial class VarEditor : UserControl
+    {
+        public string Varname;
+        public string VarValue;
+        public VarEditor(string label, string value)
+        {
+            InitializeComponent();
+            label1.Text = label;
+            textBox1.Text = value;
+            Varname = label;
+            VarValue = value;
+        }
+    }
+}
